523c974f98c8ffb5c60cdf20846a473fb9bafc9e
Sankar P <sankarp@CLLAP-CHN-1576.local>
2025-09-13 23:48:20 +0530

    script changes

--- Utils/mailler.js
+++ Utils/mailler.js
@@ -4,10 +4,23 @@ import fs from 'fs'
 export async function sendMail(subject, text) {
 const history = fs.existsSync('./Data/priceHistory.json')
 ? JSON.parse(fs.readFileSync('./Data/priceHistory.json')): []
+let cheapestRecord = history[0]
+history.forEach(rec => {
+    const price = parseFloat(rec.Price.replace(/[^0-9.]/g , ""))
+    const chepeastPrice = parseFloat(cheapestRecord.Price.replace(/[^0-9.]/g , ""))
+    if(price<chepeastPrice){
+
+        cheapestRecord = rec
+
+    }
+});
+const chepeast = cheapestRecord.Price
+const  cheapDate = cheapestRecord.timestamp
 const historyText = history.map((rec,i)=>
     ` ${i+1} | Product name : ${rec.product}  | Price  :  ${rec.Price}  | Date :  ${rec.timestamp}`
 ).join("\n")
-const finalBody = `${text} \n\n Price History :\n${historyText}`
+const finalBody = `${text} \n\n Price History :\n${historyText}, \n\n chepest record ${chepeast} on ${cheapDate}`
+
 
 
 
